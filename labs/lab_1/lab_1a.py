a3da6a778844517a468b9966d757b2a5690d5bf2
saniya-koeller <saniya.koeller@gmail.com>
2026-03-28 23:01:56 -0400

    Update lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Saniya Koeller" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("This is my first time using Git and GitHub!")
 
 if __name__ == "__main__":
     main()
